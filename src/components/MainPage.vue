acf65583fe9d88a9a42cfd386b6ab6e2d44cb49b
Liu Ruohui <1829504353@qq.com>
2022-03-03 22:53:06 +0800

    update the MainPage.vue

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,25 +1,25 @@
 <template>
 <div class="flex flex-row sm:mx-16 sm:my-20 sm:text-xl bg-indigo-300">
-    <div class="sm:basis-44">
+    <div class="sm:basis-44 ">
         <div class="grid grid-rows-3 sm:gap-6 gap-3">
             <div class="row-span-2">
                 <img src="../assets/用户.svg" alt="" class="mt-2 h-8 sm:mt-4 sm:h-32 w-2/3 mx-auto">
             </div>
             <div class="sm:text-3xl text-center m-auto">{{nickname}}</div>
-            <div class="grid grid-rows-4 gap-16 bg-indigo-400">
-                <div class="">
-                    <img src="../assets/设置.svg" alt="设置" class="h-16 m-auto"> 
-                    <p class="sm:text-3xl text-xl text-center mt-2">设置</p>
-                </div>
-                <div class="">
+            <div class="grid grid-rows-4 gap-16 bg-indigo-300">
+                <div class="bg-indigo-400" @click="gotoFriends">
                     <img src="../assets/好友.svg" alt="好友" class="h-16 m-auto">
                     <p class="sm:text-3xl text-xl text-center mt-2">好友</p>
                 </div>
-                <div class="">
+                <div class="bg-indigo-400" @click="gotoGroups">
                     <img src="../assets/群组.svg" alt="群组" class="h-16 m-auto">
                     <p class="sm:text-3xl text-xl text-center mt-2">群组</p>
                 </div>
-                <div class="">
+                <div class="bg-indigo-400" @click="settings">
+                    <img src="../assets/设置.svg" alt="设置" class="h-16 m-auto"> 
+                    <p class="sm:text-3xl text-xl text-center mt-2">设置</p>
+                </div>
+                <div class="bg-indigo-400" @click="Logout">
                     <img src="../assets/退出.svg" alt="退出" class="h-16 m-auto">
                     <p class="sm:text-3xl text-xl text-center mt-2">退出登录</p>
                 </div>
@@ -40,4 +40,20 @@
 <script setup>
 import {ref} from "vue"
 const nickname = ref('刘若晖')
+
+function settings(){
+    alert('你点击了设置')
+}
+
+function gotoFriends(){
+    alert('显示好友列表')
+}
+
+function gotoGroups(){
+    alert('显示群组列表')
+}
+
+function Logout(){
+    alert('退出登录!')
+}
 </script>
