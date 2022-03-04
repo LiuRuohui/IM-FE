c99185f6b769fe9ae14008a4d18eb41ccd5707a8
Liu Ruohui <1829504353@qq.com>
2022-03-04 22:22:45 +0800

    update the register page and cancel the form

--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -2,20 +2,20 @@
 <div class="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8c">
     <div class="sm:max-w-xl sm:object-center sm:object-cover w-full space-y-8">
         <div>
-            <h2 class="mt-6 text-center text-3xl font-extrabold text-purple-600">注册</h2>
+            <h2 class="mt-6 text-center sm:text-4xl font-extrabold text-purple-600">注册</h2>
         </div>
         <div>
             <img class="h-16 w-1/2 mx-auto" src="../assets/用户.svg" alt="这里有一张图片">
         </div>
-    <form class="mt-8 space-y-4" action="#" method="POST">
+    <div class="mt-8 space-y-4">
       <div class="rounded-md shadow-sm sm:text-xl">
         <div class="py-4 text-blue-800">
-          <label for="nickname">昵称:</label>
-          <input id="nickname" name="nickname" type="nickname" autocomplete="nickname" required class="inputFrame rounded-t-md  sm:text-xl" placeholder="请输入昵称">
+          <label for="email-address">邮箱</label>
+          <input id="email-address" name="email" type="email" autocomplete="email" required class="inputFrame rounded-t-md  sm:text-xl" placeholder="请输入邮箱">
         </div>
         <div class="py-4 text-blue-800">
-          <label for="account">账号:</label>
-          <input id="account" name="account" type="account" autocomplete="current-account" required class="inputFrame rounded-b-md sm:text-xl" placeholder="请输入账号">
+          <label for="name">昵称:</label>
+          <input id="name" name="name" type="name" autocomplete="name" required class="inputFrame rounded-t-md  sm:text-xl" placeholder="请输入昵称">
         </div>
         <div class="py-4 text-blue-800">
           <label for="passwd">密码:</label>
@@ -23,7 +23,7 @@
         </div>
         <div class="py-4 text-blue-800">
           <label for="passwd">再次输入密码:</label>
-          <input id="passwd" name="passwd" type="password" autocomplete="current-password" required class="inputFrame rounded-b-md sm:text-xl" placeholder="请再次输入密码">
+          <input id="repeated-passwd" name="repeated-passwd" type="password" autocomplete="current-password" required class="inputFrame rounded-b-md sm:text-xl" placeholder="请再次输入密码">
         </div>
       </div>
 
@@ -39,14 +39,31 @@
       <div class="sm:text-xl sm:mt-8">
         <input value="重置" type="reset" @click="reset" class="btn group sm:text-xl text-center">
         <br>
-        <input @click="submit" type="submit" class="btn group sm:text-xl">
+        <input @click="submit" type="submit" class="btn group sm:text-xl"><br>
       </div>
-    </form>
+    </div>
     </div>
 </div>
 </template>
 
 <script setup>
+function isEmail(str){
+    var reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
+    return reg.test(str)
+}
+
+function submit(){
+  let email = document.getElementById("email-address")
+  let name = document.getElementById("passwd").value
+  let repeated_name = document.getElementById("repeated-passwd").value
+  if(!isEmail(email)){
+    alert("邮箱格式不正确！")
+  }else if(name !== repeated_name){
+    alert('两次密码不一致！')
+  }else{
+    alert('提交数据注册 注册成功返回警示框 注册失败返回原因')
+  }
+}
 
 function seeFunctions(){
   alert("See functions")
@@ -56,14 +73,8 @@ function reset() {
   var confirmation = confirm('确定要重置数据吗？')
   if(confirmation == true){
     alert('你表单没了')
+    
   }
-  else{
-    alert('你表单还是没了')
-  }
-}
-
-function submit() {
-  alert('提交数据注册 注册成功返回警示框 注册失败返回原因')
 }
 
 </script>
