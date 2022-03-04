41e9326caf2513abe7a725b3045961bf4b46d9b0
Liu Ruohui <1829504353@qq.com>
2022-03-04 22:26:59 +0800

    update the loginpage and cancel the form

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -2,47 +2,58 @@
 <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
   <div class="max-w-md w-full space-y-8">
     <div>
-      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">登录</h2>
+      <h2 class="mt-6 text-center sm:text-4xl font-extrabold text-gray-900">登录</h2>
     </div>
     <div>
         <img class="h-16 w-1/2 mx-auto" src="../assets/用户.svg" alt="这里有一张图片">
     </div>
-    <form class="mt-8 space-y-6" action="#" method="POST">
+    <div class="mt-8 space-y-6">
       <input type="hidden" name="remember" value="true">
       <div class="rounded-md shadow-sm -space-y-px">
-        <div class="py-4 text-blue-800 sm:text-xl">
-          <label for="account">账号:</label>
-          <input id="account" name="account" type="account" autocomplete="current-account" required class="inputFrame rounded-b-md sm:text-xl" placeholder="请输入账号">
+        <div class="py-4 text-blue-800 sm:text-2xl">
+          <label for="email-address">邮箱</label>
+          <input id="email-address" name="email" type="email" autocomplete="email" required class="inputFrame rounded-t-md  sm:text-2xl" placeholder="请输入邮箱">
         </div>
-        <div class="py-4 text-blue-800 sm:text-xl">
+        <div class="py-4 text-blue-800 sm:text-2xl">
           <label for="passwd">密码:</label>
-          <input id="passwd" name="passwd" type="password" autocomplete="current-password" required class="inputFrame rounded-b-md  sm:text-xl" placeholder="请输入密码">
+          <input id="passwd" name="passwd" type="password" autocomplete="current-password" required class="inputFrame rounded-b-md  sm:text-2xl" placeholder="请输入密码">
         </div>
       </div>
 
       <div class="flex items-center justify-between">
         <div class="flex items-center">
           <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-          <label for="remember-me" class="ml-2 block text-sm sm:text-xl text-gray-900"> 记住我</label>
+          <label for="remember-me" class="ml-2 block text-sm sm:text-2xl text-gray-900"> 记住我</label>
         </div>
 
         <div class="text-sm">
-          <a href="#" class="font-medium sm:text-xl text-indigo-600 hover:text-indigo-500"> 忘记密码? </a>
+          <a href="#" class="font-medium sm:text-2xl text-indigo-600 hover:text-indigo-500"> 忘记密码? </a>
         </div>
       </div>
 
       <div>
-        <button @click="submit" type="submit" class="btn group sm:text-xl">
+        <button @click="submit" class="btn group sm:text-xl">
           登录
         </button>
       </div>
-    </form>
+    </div>
   </div>
 </div>
 </template>
 
-<script>
-
+<script setup>
+function submit(){
+  var name = document.getElementById("email-address").value;
+  if(!isEmail(name)){
+    alert('邮箱格式不正确！')
+  }else{
+    alert('登录')
+  }
+}
+function isEmail(str){
+    var reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
+    return reg.test(str)
+}
 </script>
 
 
