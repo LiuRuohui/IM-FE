9a2ab8145642e1ee2578a24ab323b2b8cda8739e
yingyi <jinzhongjia@qq.com>
2022-03-18 08:48:24 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import Login from "./components/Login.vue"
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <router-link to="/">home</router-link>
     <router-link to="/login">login</router-link>
     <router-link to="/info">info</router-link>
@@ -11,10 +11,11 @@
     <router-link to="/register">register</router-link>
     <router-link to="/main">main</router-link>
     <router-link to="/forgetPasswd">forgetPasswd</router-link>
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
-  </div>
+  </div>-->
+  <!-- <keep-alive>
+    <router-view></router-view>
+  </keep-alive>-->
+  <login></login>
 </template>
 
 <style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,82 +1,115 @@
 <template>
- <div class="relative min-h-screen flex">
-      <div class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 bg-white">
-          <div class="sm:w-1/3 xl:w-3/5 h-full hidden md:flex flex-auto items-center justify-center p-10 overflow-hidden bg-purple-900 text-white bg-no-repeat bg-cover relative" style="background-image: url(img.jpg)">
-              <div class="absolute bg-gradient-to-b from-indigo-600 to-blue-500 opacity-75 inset-0 z-0"></div>
-                    <div class="w-full max-w-md z-10">
-                        <div class="sm:text-4xl font-bold leading-tight mb-6">
-                          欢迎光临
-                        </div>
-                        <div class="sm:text-xl text-gray-200 font-normal">
-                          欢迎来到PDD和yingyi的即时通讯程序,您能够在里面感受到优质的界面设计,感受到良好的单聊群聊氛围,感受科技带给我们的变革,体会http和ws对通讯的技巧...
-                        </div>
-                    </div>
-                    <ul class="circles">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+    <div class="relative min-h-screen flex">
+        <div
+            class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 bg-white"
+        >
+            <div
+                class="sm:w-1/3 xl:w-3/5 h-full hidden md:flex flex-auto items-center justify-center p-10 overflow-hidden bg-purple-900 text-white bg-no-repeat bg-cover relative"
+                style="background-image: url(/src/assets/pic/img.jpg)"
+            >
+                <div
+                    class="absolute bg-gradient-to-b from-indigo-600 to-blue-500 opacity-75 inset-0 z-0"
+                ></div>
+                <div class="w-full max-w-md z-10">
+                    <div class="sm:text-4xl xl:text-5xl font-bold leading-tight mb-6">欢迎光临</div>
+                    <div
+                        class="sm:text-sm xl:text-md text-gray-200 font-normal"
+                    >欢迎来到好玩俱乐部，在这里和志同道合的朋友一起分享有趣的故事，一起组织有趣的活动...</div>
                 </div>
-                <div class="md:flex md:items-center md:justify-center w-full sm:w-auto md:h-full xl:w-2/5 p-8 md:p-10 lg:p-14 sm:rounded-lg md:rounded-none bg-white">
-                    <div class="max-w-md w-full mx-auto space-y-8">
-                        <div class="text-center">
-                            <h2 class="mt-6 text-3xl font-bold text-gray-900">欢迎回来</h2>
-                        </div>
-                        <div class="object-center sm:mt-8">
-                            <img class="h-16 w-1/2 mx-auto" src="../assets/pic/用户.svg" alt="这里有一张图片">
-                        </div>
-                        <div class="mt-8 space-y-6">
-                            <input type="hidden" name="remember" value="true" />
-                            <div class="rounded-md shadow-sm">
-                                <div class="py-2 font">
-                                    <label for="account">账号:</label>
-                                    <input id="account" name="account" type="account" autocomplete="name" required class="inputFrame rounded-t-md  sm:text-xl" placeholder="请输入账号">
-                                  </div>
-                                  <div class="py-2 font">
-                                    <label for="passwd">密码:</label>
-                                    <input id="passwd" name="passwd" type="password" autocomplete="current-password" required class="inputFrame rounded-b-md  sm:text-xl" placeholder="请输入密码">
-                                  </div>
-                                </div>
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center">
-                                    <input id="remember_me" name="remember_me" type="checkbox" class="h-4 w-4 bg-blue-500 focus:ring-blue-400 border-gray-300 rounded "/>
-                                    <label for="remember_me" class="ml-2 block text-sm text-gray-900" >
-                                      记住我
-                                    </label>
-                                </div>
-                                <div class="text-sm">
-                                    <a href="#" class="text-indigo-400 hover:text-blue-500">
-                                      忘记密码？
-                                    </a>
-                                </div>
+                <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div
+                class="md:flex md:items-center md:justify-center w-full sm:w-auto md:h-full xl:w-2/5 p-8 md:p-10 lg:p-14 sm:rounded-lg md:rounded-none bg-white"
+            >
+                <div class="max-w-md w-full mx-auto space-y-8">
+                    <div class="text-center">
+                        <h2 class="mt-6 text-3xl font-bold text-gray-900">欢迎回来</h2>
+                    </div>
+                    <div class="flex items-center justify-center space-x-2"></div>
+                    <div class="mt-8 space-y-6">
+                        <input type="hidden" name="remember" value="true" />
+                        <div class="rounded-md shadow-sm">
+                            <div class="py-2 font">
+                                <label
+                                    class="ml-3 text-sm font-bold text-gray-700 tracking-wide"
+                                    for="account"
+                                >账号:</label>
+                                <input
+                                    id="account"
+                                    name="account"
+                                    type="account"
+                                    autocomplete="name"
+                                    required
+                                    class="w-full text-base px-4 py-2 border-b border-gray-300 focus:outline-none rounded-2xl focus:border-indigo-500"
+                                    placeholder="请输入账号"
+                                />
                             </div>
-                            <div>
-                                <button @click="submit" class="btn group sm:text-2xl">
-                                    登录
-                                </button>
+                            <div class="py-2 font">
+                                <label
+                                    class="ml-3 text-sm font-bold text-gray-700 tracking-wide"
+                                    for="passwd"
+                                >密码:</label>
+                                <input
+                                    id="passwd"
+                                    name="passwd"
+                                    type="password"
+                                    autocomplete="current-password"
+                                    required
+                                    class="w-full content-center text-base px-4 py-2 border-b rounded-2xl border-gray-300 focus:outline-none focus:border-indigo-500"
+                                    placeholder="请输入密码"
+                                />
                             </div>
-                            <p class="items-center justify-center mt-10 text-center text-md text-gray-500">
-                                <span>还没有账号？</span>
-                                <a href="https://www.dowebok.com/7948.html" target="_blank" class="text-indigo-400 hover:text-blue-500 no-underline hover:underline cursor-pointer transition ease-in duration-300">
-                                    立即注册
-                                </a>
-                            </p>
                         </div>
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-center">
+                                <input
+                                    id="remember_me"
+                                    name="remember_me"
+                                    type="checkbox"
+                                    class="h-4 w-4 bg-blue-500 focus:ring-blue-400 border-gray-300 rounded"
+                                />
+                                <label
+                                    for="remember_me"
+                                    class="ml-2 block text-sm text-gray-900"
+                                >记住我</label>
+                            </div>
+                            <div class="text-sm">
+                                <a href="#" class="text-indigo-400 hover:text-blue-500">忘记密码？</a>
+                            </div>
+                        </div>
+                        <div>
+                            <button
+                                @click="submit"
+                                class="w-full flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500"
+                            >登录</button>
+                        </div>
+                        <p
+                            class="items-center justify-center mt-10 text-center text-md text-gray-500"
+                        >
+                            <span>还没有账号？</span>
+                            <a
+                                href="/"
+                                target="_blank"
+                                class="text-indigo-400 hover:text-blue-500 no-underline hover:underline cursor-pointer transition ease-in duration-300"
+                            >立即注册</a>
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </template>
 
 <style>
-.font{
-  color:hsla(210, 100%, 60%, 0.8)
-}
 .circles {
     position: absolute;
     top: 0;
@@ -173,5 +206,4 @@
         border-radius: 50%;
     }
 }
-
 </style>
