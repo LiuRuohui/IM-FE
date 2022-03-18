df98a9ab2435c3d19e292fe183f122ea9231ecf8
yingyi <jinzhongjia@qq.com>
2022-03-18 17:50:32 +0800

    字体微调

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -43,7 +43,7 @@ function page(param) {
                         class="sm:text-4xl xl:text-5xl font-bold leading-tight mb-6 select-none"
                     >欢迎光临</div>
                     <div
-                        class="sm:text-xl xl:text-md text-gray-200 font-normal select-none"
+                        class="sm:text-sm xl:text-md text-gray-200 font-normal"
                     >欢迎来到好玩俱乐部，在这里和志同道合的朋友一起分享有趣的故事，一起组织有趣的活动...</div>
                 </div>
                 <ul class="circles">
