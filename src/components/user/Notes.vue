71bb1bed277687088400e8b4dde7786b7700cea9
Liu Ruohui <1829504353@qq.com>
2022-03-19 16:41:17 +0800

    完成记事功能修改

--- src/components/user/Notes.vue
+++ src/components/user/Notes.vue
@@ -6,7 +6,7 @@ const notebooks = reactive({
 })
 </script>
 <template>
-    <div class="w-96 h-full flex flex-col">
+    <div class="w-96 h-full flex flex-col overflow-hidden">
         <div class="w-full flex flex-row my-2">
             <div class="opacity-60 float-left ml-4 mt-1">
                 列表排序
@@ -15,7 +15,7 @@ const notebooks = reactive({
                     <option value="">倒序</option>
                 </select>
             </div>
-            <div class="ml-16 opacity-60">
+            <div class="ml-14 opacity-60">
                 创建会话
                 <div class="inline-block item-center">
                     <img src="/src/assets/img/加号.svg" alt="" class="h-9 inline-block">
@@ -23,14 +23,12 @@ const notebooks = reactive({
             </div>
         </div>
         <div class="w-full h-full">
-            <div class="flex flex-col my-4 mx-6 opacity-80 overflow-hidden">
-                <div class="w-full h-28 shadow-md rounded-xl" v-for="notes in notebooks.id">
-                    <img src="/src/assets/pic/450824.jpg" alt="" class="my-4 h-20 w-20 inline-block">
-                    <div class="inline-block mt-">
-                        <p class="font-semibold ml-3 inline-block">笔记标题</p>
-                        <div class="inline-block float-right">2020-6-22</div>
-                        <p class="mt-1 ml-2 w-full text-sm opacity-70">This is one of the notes This</p>
-                    </div>
+            <div class="flex flex-col my-4 mx-8">
+                <div class="w-full h-28 shadow-md hover:shadow-lg rounded-md overflow-auto" v-for="notes in notebooks.id">
+                    <img src="/src/assets/pic/450824.jpg" alt="" class="ml-4 my-4 h-20 w-20 inline-block float-left">
+                        <div class="float-left ml-3 mt-6 font-bold opacity-70 hover:opacity-100">笔记标题</div>
+                        <div class="hover:font-bold float-right mt-6 opacity-50 mr-4">2020-6-22</div>
+                        <p class="inline-block ml-3 truncate opacity-70 text-sm mt-2 hover:opacity-90">This is a note</p>
                 </div>
             </div>
         </div>
