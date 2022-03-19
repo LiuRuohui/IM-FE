aec3d19656dbc7822a1bef7127dfa2d2a1141dfd
Liu Ruohui <1829504353@qq.com>
2022-03-19 18:54:55 +0800

    change the style from float to flex

--- src/components/user/Notes.vue
+++ src/components/user/Notes.vue
@@ -1,14 +1,14 @@
 <script setup>
 import { reactive } from 'vue';
 const notebooks = reactive({
-    id: [1, 2, 3, 4, 5, 6],
+    id: [1,2,3,4,5,6],
     message: [],
 })
 </script>
 <template>
     <div class="w-96 h-full flex flex-col overflow-hidden select-none">
         <div class="w-full flex flex-row my-2">
-            <div class="opacity-60 float-left ml-4 mt-1">
+            <div class="opacity-60 ml-4 mt-1">
                 列表排序
                 <select name id class="border-none outline-none font-black text-center">
                     <option value>最新笔记优先</option>
@@ -25,19 +25,23 @@ const notebooks = reactive({
         <div class="w-full h-full">
             <div class="flex flex-col my-4 mx-8">
                 <div
-                    class="w-full h-28 shadow-sm hover:shadow rounded-md hover:cursor-pointer"
+                    class="group flex flex-row w-full h-28 shadow-sm hover:shadow rounded-md hover:cursor-pointer"
                     v-for="notes in notebooks.id"
                 >
                     <img
                         src="/src/assets/pic/450824.jpg"
                         alt
-                        class="ml-4 my-4 h-20 w-20 inline-block float-left"
+                        class="ml-4 my-4 h-20 w-20 inline-block"
                     />
-                    <div class="float-left ml-3 mt-6 font-bold opacity-70">笔记标题</div>
-                    <div class="font-semibold float-right mt-6 opacity-50 mr-4">2020-6-22</div>
-                    <div
-                        class="inline-block ml-3 truncate opacity-70 text-sm mt-2 group-hover:opacity-100"
-                    >This is a note</div>
+                    <div class="flex flex-col">
+                        <div class="flex flex-row">
+                            <div class="ml-3 mt-6 font-bold text-base opacity-70 group-hover:opacity-90">笔记标题</div>
+                            <div class="font-semibold mt-6 opacity-50 group-hover:opacity-90 ml-12 text-sm">2020-6-22</div>
+                        </div>
+                        <div
+                            class="inline-block ml-3 truncate opacity-70 text-sm mt-2 group-hover:opacity-100"
+                        >This is a note</div>
+                    </div>
                 </div>
             </div>
         </div>
