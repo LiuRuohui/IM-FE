a4ddcb985e9a3e7bda2f00b0dd3eb34594493265
yingyi <jinzhongjia@qq.com>
2022-03-23 12:19:13 +0800

    增加打包分析

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		visualizer({
+			filename: "./dist/status.html",
+			open: false,
+			template: "treemap",
+			// @ts-ignore
+			gzipSize: true,
+			// @ts-ignore
+			brotliSize: true,
+		}),
+	],
 	server: {
 		host: "0.0.0.0",
 	},
