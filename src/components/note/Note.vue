9e91bf9e153375066de0f7c95902dad809053148
Liu Ruohui <1829504353@qq.com>
2022-03-23 12:43:59 +0800

    increase the Note.vue

---
+++ src/components/note/Note.vue
@@ -0,0 +1,68 @@
+<script setup>
+import {reactive,ref,onMounted} from "vue"
+const notebooks = reactive({
+    id: [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12, 13, 14, 15],
+    message: [],
+});
+
+const height = ref("0px")
+const noteContainer = ref(null)
+
+onMounted(() => {
+    height.value = noteContainer.value.offsetHeight + "px"
+})
+
+</script>
+
+<template>
+<div class="flex-grow h-full flex flex-wrap overflow-y-auto relative">
+    <div class="w-full md:w-96 h-full flex flex-col overflow-hidden select-none border-r border-gray-200">
+        <div class="w-full h-16 flex justify-center items-center my-2 border-b border-gray-100">
+            <div class="w-5/6 relative">
+                <input class="box-border rounded-full h-9 pl-14 pr-4 py-3 w-full outline-none bg-gray-100 text-sm select-none" type="text" placeholder="请输入搜索的内容"/>
+                <img class="box-content w-5 h-5 absolute top-2 left-6  border-gray-300 drag" src="/src/assets/img/search.svg"/>
+            </div>
+        </div>
+        <div class="w-5/6 flex mx-auto">
+            <div class="opacity-60 mt-1 flex-1">
+                列表排序:
+                <select class="outline-none border-none font-bold text-center">
+                    <option value>新添加优先</option>
+                    <option value>后添加优先</option>
+                </select>
+            </div>
+            <div class="opacity-60">
+                 创建会话
+                <div class="inline-block item-center">
+                    <img class="h-9 inline-block" src="/src/assets/img/加号.svg" />
+                </div>
+            </div>
+        </div>
+        <div class="w-full flex-grow" ref="noteContainer">
+            <div class="w-full overflow-y-auto no-scrollbar" :style="{ height: height }">
+                <div class="flex flex-col my-4 mx-8">
+                    <div class="group flex items-center w-full h-24 shadow-sm hover:shadow hover:cursor-pointer md:px-2 border-x-4 border-y-8 border-slate-50"
+                         v-for="notes in notebooks.id">
+                        <div class="mx-4 w-10 h-10 md:w-12 md:h-12 rounded-full my-4 m-auto relative box-border group-hover:border-2 border-blue-500">
+                            <div class="absolute top-0.5 w-3 h-3 md:w-4 md:h-4 rounded-full bg-green-600 border-4 border-slate-100"></div>
+                            <img class="rounded-full" src="/src/assets/avatar/squidWard.jpg" alt="章鱼哥" />
+                        </div>
+                        <div class="flex flex-col flex-grow h-full justify-center pl-2 pr-3">
+                            <div class="flex h-1/2 pt-6">
+                                <div class="font-bold text-base opacity-70 group-hover:opacity-90 flex items-center">
+                                    楚中天
+                                </div>
+                            </div>
+                            <div class="inline-block truncate opacity-40 text-sm group-hover:opacity-70 h-1/2 pt-1">
+                                 前端工程师
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+<style>
+</style>
--- src/router/route.js
+++ src/router/route.js
@@ -1,49 +1,54 @@
 const routes = [
-	// 路由的默认路径
-	{
-		path: "/",
-		name: "welcome",
-		component: () => import("../components/Welcome.vue"),
-	},
-	{
-		path: "/login",
-		name: "login",
-		component: () => import("../components/login/Login.vue"),
-		meta: {
-			title: "登录注册",
-		},
-	},
-	{
-		path: "/preview",
-		name: "preview",
-		component: () => import("../components/preview/Preview.vue"),
-		meta: {
-			title: "预览",
-		},
-	},
-	{
-		path: "/public",
-		name: "public",
-		component: () => import("../components/Public.vue"),
-		children: [
-			{
-				path: "setting",
-				name: "setting",
-				component: () => import("../components/setting/Setting.vue"),
-				meta: {
-					title: "个人设置",
-				},
-			},
-			{
-				path: "note",
-				name: "note",
-				component: () => import("../components/note/Notes.vue"),
-				meta: {
-					title: "我的笔记",
-				},
-			},
-		],
-	},
+    // 路由的默认路径
+    {
+        path: "/",
+        name: "welcome",
+        component: () =>
+            import ("../components/Welcome.vue"),
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: () =>
+            import ("../components/login/Login.vue"),
+        meta: {
+            title: "登录注册",
+        },
+    },
+    {
+        path: "/preview",
+        name: "preview",
+        component: () =>
+            import ("../components/preview/Preview.vue"),
+        meta: {
+            title: "预览",
+        },
+    },
+    {
+        path: "/public",
+        name: "public",
+        component: () =>
+            import ("../components/Public.vue"),
+        children: [{
+                path: "setting",
+                name: "setting",
+                component: () =>
+                    import ("../components/setting/Setting.vue"),
+                meta: {
+                    title: "个人设置",
+                },
+            },
+            {
+                path: "note",
+                name: "note",
+                component: () =>
+                    import ("../components/note/Note.vue"),
+                meta: {
+                    title: "我的笔记",
+                },
+            },
+        ],
+    },
 ]
 
 export { routes }
