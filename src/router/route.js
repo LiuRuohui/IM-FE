254139b2f5ffadd8bdb5a375f44f1e896858fae3
Liu Ruohui <1829504353@qq.com>
2022-03-30 09:19:09 +0800

    消息部分基本完成

--- src/router/route.js
+++ src/router/route.js
@@ -1,57 +1,81 @@
 const routes = [
-	// 路由的默认路径
-	{
-		path: "/",
-		name: "welcome",
-		component: () => import("../components/Welcome.vue"),
-	},
-	{
-		path: "/login",
-		name: "login",
-		component: () => import("../components/login/Login.vue"),
-		meta: {
-			title: "登录注册",
-		},
-	},
-	{
-		path: "/preview",
-		name: "preview",
-		component: () => import("../components/preview/Preview.vue"),
-		meta: {
-			title: "预览",
-		},
-	},
-	{
-		path: "/public",
-		name: "public",
-		component: () => import("../components/Public.vue"),
-		children: [
-			{
-				path: "chum",
-				name: "chum",
-				component: () => import("../components/chum/Chum.vue"),
-				meta: {
-					title: "我的好友",
-				},
-			},
-			{
-				path: "note",
-				name: "note",
-				component: () => import("../components/note/Notes.vue"),
-				meta: {
-					title: "我的笔记",
-				},
-			},
-			{
-				path: "setting",
-				name: "setting",
-				component: () => import("../components/setting/Setting.vue"),
-				meta: {
-					title: "个人设置",
-				},
-			},
-		],
-	},
+    // 路由的默认路径
+    {
+        path: "/",
+        name: "welcome",
+        component: () =>
+            import ("../components/Welcome.vue"),
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: () =>
+            import ("../components/login/Login.vue"),
+        meta: {
+            title: "登录注册",
+        },
+    },
+    {
+        path: "/preview",
+        name: "preview",
+        component: () =>
+            import ("../components/preview/Preview.vue"),
+        meta: {
+            title: "预览",
+        },
+    },
+    {
+        path: "/public",
+        name: "public",
+        component: () =>
+            import ("../components/Public.vue"),
+        children: [{
+                path: "msg",
+                name: "msg",
+                component: () =>
+                    import ("../components/msg/Msg.vue"),
+                meta: {
+                    title: "我的信息",
+                },
+            },
+            {
+                path: "chum",
+                name: "chum",
+                component: () =>
+                    import ("../components/chum/Chum.vue"),
+                meta: {
+                    title: "我的好友",
+                },
+            },
+            {
+                path: "file",
+                name: "file",
+                component: () =>
+                    import ("../components/file/File.vue"),
+                meta: {
+                    title: "我的文件",
+                },
+            },
+            {
+                path: "note",
+                name: "note",
+                component: () =>
+                    import ("../components/note/Notes.vue"),
+                meta: {
+                    title: "我的笔记",
+                },
+            },
+            {
+                path: "setting",
+                name: "setting",
+                component: () =>
+                    import ("../components/setting/Setting.vue"),
+                meta: {
+                    title: "个人设置",
+                },
+            },
+        ],
+    },
 ]
 
 export { routes }
