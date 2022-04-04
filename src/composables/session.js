0effbb66638bb3d84c30333bc2bdb10b077bba31
yingyi <jinzhongjia@qq.com>
2022-04-04 21:02:23 +0800

    微调

--- src/composables/session.js
+++ src/composables/session.js
@@ -2,14 +2,15 @@
 创建session.js 存在一个对象两个方法分别为读取和修改 外部不暴露session 通过对象的方法访问 暴露对象在外
  */
 var storage = localStorage.getItem("Session-Id")
-var session = ({
-    setSessionId(sessionId) {
-        //设置sessionId
-        storage = sessionId
-    },
-    getSessionId() {
-        return storage
-    }
-})
+var session = {
+	setSessionId(sessionId) {
+		//设置sessionId
+		storage = sessionId
+		localStorage.setItem("Session-Id", sessionId)
+	},
+	getSessionId() {
+		return storage
+	},
+}
 
 export { session }
