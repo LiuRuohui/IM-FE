46aeab49b04c6f3978900d85632042586976fedc
yingyi <jinzhongjia@qq.com>
2022-04-08 00:10:29 +0800

    重新定义http文件

--- src/composables/http.js
+++ src/composables/http.js
@@ -2,98 +2,104 @@ import axios from "axios"
 import QS from "qs"
 
 import { session } from "/src/composables/session"
-import { url } from "./env"
 
 const instance = axios.create({
-    // `baseURL` 将自动加在 `url` 前面，除非 `url` 是一个绝对 URL。
-    // 它可以通过设置一个 `baseURL` 便于为 axios 实例的方法传递相对 URL
-    baseURL: 'http://api.jinzh.me:8976',
-    // `timeout` 指定请求超时的毫秒数。
-    // 如果请求时间超过 `timeout` 的值，则请求会被中断
-    timeout: 3000, // 默认值是 `0` (永不超时)
-    // 自定义请求头
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-    },
-
-    get(url, params) {
-        // url参数以对象形式传入
-        return new Promise((resolve, reject) => {
-            instance
-                .get(url, {
-                    params: params,
-                })
-                .then((res) => {
-                    resolve(res.data)
-                })
-                .catch((err) => {
-                    reject(err.data)
-                })
-        })
-    },
-    post(url, params) {
-        //表单以对象形式传入
-        return new Promise((resolve, reject) => {
-            instance
-                .post(url, QS.stringify(params), {
-                    headers: {
-                        // "Content-Type": "application/x-www-form-urlencoded",
-                    },
-                })
-                .then((res) => {
-                    resolve(res.data)
-                    console.log(res.data)
-                })
-                .catch((err) => {
-                    reject(err.data)
-                })
-        })
-    },
+	// `baseURL` 将自动加在 `url` 前面，除非 `url` 是一个绝对 URL。
+	baseURL: "http://api.jinzh.me:8976",
+	// `timeout` 指定请求超时的毫秒数。
+	timeout: 3000, // 默认值是 `0` (永不超时)
+	// 自定义请求头
+	headers: {
+		"Content-Type": "application/x-www-form-urlencoded",
+		//自定义的header头部存储sessionId
+		"Session-Id": session.getSessionId(),
+	},
 })
 
-
+// 请求拦截器
 instance.interceptors.request.use(
-    //请求拦截器
-    function(config) {
-        // 在发送请求之前将登录获得的Session-ID存到localStorage中
-        config.headers['Session-Id'] = session.getSessionId()
-        return config
-    },
-    function(error) {
-        // 对请求错误做些什么
-        return Promise.reject(error)
-    }
+	//请求拦截器
+	function (config) {
+		return config
+	},
+	function (error) {
+		// 对请求错误做些什么
+		return Promise.reject(error)
+	}
 )
 
+// 相应拦截器
 instance.interceptors.response.use(
-    function(response) {
-        // 2xx 范围内的状态码都会触发该函数。
-        // 对响应数据做点什么
-        return response
-    },
-    function(error) {
-        // 超出 2xx 范围的状态码都会触发该函数。
-        // 对响应错误做点什么
-        switch (error.response.status) {
-            case 401:
-                console.log("未登录！")
-                break
-            case 404:
-                console.log("错误路径，资源未找到！")
-                break
-            case 403:
-                console.log("禁止访问！")
-                break
-            case 405:
-                console.log("不支持的方法")
-                break
-                // case ...
-            default:
-                console.log("其他错误")
-                break
-        }
-        return Promise.reject(error)
-    }
+	function (response) {
+		// 2xx 范围内的状态码都会触发该函数。
+		// 对响应数据做点什么
+		return response
+	},
+	function (error) {
+		// 超出 2xx 范围的状态码都会触发该函数。
+		// 对响应错误做点什么
+		switch (error.response.status) {
+			case 401:
+				console.log("未登录！")
+				break
+			case 403:
+				console.log("禁止访问！")
+				break
+			case 404:
+				console.log("错误路径，资源未找到！")
+				break
+
+			case 405:
+				console.log("不支持的方法")
+				break
+			// case ...
+			default:
+				console.log("出现错误")
+				break
+		}
+		return Promise.reject(error)
+	}
 )
 
-export { instance }
+//封装好对外暴露的http对象
+const http = {}
+
+// 封装好的get方法
+http.get = function (url, params, sessionId) {
+	//这里的sessionId可以用来添加token
+	params.sessionId = sessionId
+	return new Promise((resolve, reject) => {
+		instance
+			.get(url, {
+				params: params,
+			})
+			.then((res) => {
+				resolve(res.data)
+			})
+			.catch((err) => {
+				reject(err.data)
+			})
+	})
+}
+
+// 封装好的post方法
+http.post = function (url, params, sessionId) {
+	return new Promise((resolve, reject) => {
+		instance
+			.post(url, QS.stringify(params), {
+				//这里使用qs来对project进行转码，使之成为表单内容
+				params: { sessionId: sessionId },
+				headers: {
+					// "Content-Type": "application/x-www-form-urlencoded",
+				},
+			})
+			.then((res) => {
+				resolve(res.data)
+			})
+			.catch((err) => {
+				reject(err.data)
+			})
+	})
+}
+
+export { instance, http }
