a860fc271dc54ae1462c1e40183503e45b324672
yingyi <jinzhongjia@qq.com>
2022-04-08 00:10:34 +0800

    session微调

--- src/composables/session.js
+++ src/composables/session.js
@@ -1,16 +1,22 @@
 /*
 创建session.js 存在一个对象两个方法分别为读取和修改 外部不暴露session 通过对象的方法访问 暴露对象在外
  */
-var storage = localStorage.getItem("Session-Id")
-var session = {
-	setSessionId(sessionId) {
-		//设置sessionId
-		storage = sessionId
-		localStorage.setItem("Session-Id", sessionId)
-	},
-	getSessionId() {
-		return storage
-	},
+
+// 创建session，通过localstroage获取
+var sessionId = localStorage.getItem("Session-Id")
+
+var session = {}
+
+//方法设置sessionId
+session.setSessionId = function (tmp) {
+	//设置sessionId
+	sessionId = tmp
+	localStorage.setItem("Session-Id", tmp)
+}
+
+// 方法获取sessionId
+session.getSessionId = function () {
+	return sessionId
 }
 
 export { session }
