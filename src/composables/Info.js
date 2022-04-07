a18046b32e0e1e2609b3eec09420754777596fa2
yingyi <jinzhongjia@qq.com>
2022-04-08 00:10:45 +0800

    Info暂定保留

--- src/composables/Info.js
+++ src/composables/Info.js
@@ -4,61 +4,61 @@
 保存getInfo的方法可以通过Info.getInfo()获取从数据库中存储的信息渲染在页面上
 设置debounce防抖方法使得在输入不会数据一发生改变就向服务器post结果
 */
+import { info } from "autoprefixer"
 import { reactive } from "vue"
-import { instance } from "/src/composables/http"
+import { http } from "/src/composables/http"
 const Info = reactive({
-    name: null,
-    sex: null,
-    signature: null,
-    phone: null,
-    mail: null,
-    site: null,
-    github: null,
-    weibo: null,
-    qq: null,
+	id: "",
+	name: "",
+	sex: false,
+	signature: "",
+	phone: "",
+	mail: "",
+	site: "",
+	github: "",
+	weibo: "",
+	qq: "",
 
-    getInfo() {
-        instance.get('/user/info').then(
-            response => {
-                console.log('获取成功', response.data)
-                Info.name = response.data.Name
-                Info.sex = response.data.Sex
-                Info.signature = response.data.Signature
-                Info.phone = response.data.Phone
-                Info.mail = response.data.Mail
-                Info.site = response.data.Site
-                Info.github = response.data.Github
-                Info.weibo = response.data.Weibo
-                Info.qq = response.data.Qq
-            },
-            error => {
-                console.log('获取失败', error.message)
-            }
-        )
-    },
+	getInfo() {
+		http.get("/user/info", {}, "").then(
+			(data) => {
+				console.log(data)
+				Info.name = data.Name
+				Info.sex = data.Sex
+				Info.signature = data.Signature
+				Info.phone = data.Phone
+				Info.mail = data.Mail
+				Info.site = data.Site
+				Info.github = data.Github
+				Info.weibo = data.Weibo
+				Info.qq = data.Qq
+			},
+			(error) => {}
+		)
+	},
 
-    checkSex() {
-        if (Info.sex == true) {
-            document.getElementById("male").checked = false
-            document.getElementById("female").checked = true
-        } else {
-            document.getElementById("male").checked = true
-            document.getElementById("female").checked = false
-        }
-    }
+	checkSex() {
+		if (Info.sex == true) {
+			document.getElementById("male").checked = false
+			document.getElementById("female").checked = true
+		} else {
+			document.getElementById("male").checked = true
+			document.getElementById("female").checked = false
+		}
+	},
 })
 
+//防抖函数
 function debounce(fn, delay) {
-    let timer
-    return function() {
-        if (timer) {
-            clearTimeout(timer)
-        }
-        timer = setTimeout(() => {
-            fn()
-        }, delay)
-    }
+	let timer
+	return function () {
+		if (timer) {
+			clearTimeout(timer)
+		}
+		timer = setTimeout(() => {
+			fn()
+		}, delay)
+	}
 }
-
 
 export { Info, debounce }
