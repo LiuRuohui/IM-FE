64294565401f8096b850f7a9cdeac8afbde7dfa6
Liu Ruohui <1829504353@qq.com>
2022-04-14 22:31:32 +0800

    顶部完成

--- src/components/file/File.vue
+++ src/components/file/File.vue
@@ -101,33 +101,26 @@
 		>
 			<div class="flex flex-col">
 				<div
-					class="w-full h-16 box-content overflow-hidden mx-auto flex flex-row border-1 shadow"
+					class="w-full h-24 box-content overflow-hidden mx-auto flex flex-row border-1 shadow"
 				>
 					<div
-						class="flex mx-4 sm:w-12 sm:h-12 rounded-full my-3 m-auto relative box-border"
+						class="flex mx-8 sm:w-20 sm:h-20 rounded-full my-4 m-auto relative box-border"
 					>
-						<div
-							class="absolute top-0.5 w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-green-600 border-2 border-slate-100"
-						></div>
-						<img
-							class="rounded-full sm:h-10 sm:w-10"
-							src="/src/assets/avatar/squidWard.jpg"
-							alt="章鱼哥"
-						/>
+					  <div><img src="/src/assets/img/pdf.svg" alt="" class="h-16"></div>
 					</div>
-					<div class="flex flex-col justify-center mr-4">
-						<div class="flex h-1/2 pt-4">
-							<div class="font-bold text-sm opacity-70 flex items-center">林凌</div>
-						</div>
-						<div class="truncate opacity-40 text-xs h-1/2 pt-1">在线</div>
+					<div class="flex justify-center">
+						<input type="text" placeholder="SourceCode.zip" class="appearance-none outline-none hover:cursor-pointer">
 					</div>
-					<div class="flex-grow flex flex-row-reverse py-4 pr-4">
+					<div class="flex-grow flex flex-row-reverse py-6 pr-6">
 						<div class="hover:cursor-pointer sm:w-12">
 							<img
-								class="h-10 opacity-60 hover:opacity-100"
+								class="h-12 opacity-60 hover:opacity-100"
 								src="/src/assets/img/更多.svg"
 								alt="更多"
 							/>
+						</div>
+						<div class="pr-8">
+							<div class="pt-3 opacity-60">2022-06-12</div>
 						</div>
 					</div>
 				</div>
