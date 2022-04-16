4238f8830a71c156f64fd9ed4071d5d8fa6a2fd9
Liu Ruohui <1829504353@qq.com>
2022-04-17 00:40:06 +0800

    错误代码

--- src/components/file/components/upload.vue
+++ src/components/file/components/upload.vue
@@ -1,26 +1,36 @@
 <script setup>
-import {File} from "/src/composables/api"
-function upload(){
-  let s = document.getElementById("file")
+import { File } from "/src/composables/api";
+function upload() {
+  let s = document.getElementById("file");
   let formData = new FormData();
   formData.append("file",s.files[0]);
-  console.log(formData)
-  if(s.files[0] == undefined){
-    return
+  console.log(formData.get("file"))
+  if (s.files[0] == undefined) {
+    return;
   }
-  File.upload(formData)
+  console.log(s.files[0]);
+  var reader = new FileReader(); //新建一个FileReader
+  reader.readAsText(s.files[0], "UTF-8"); //读取文件
+  reader.onload = function (evt) {
+    //读取完文件之后会回来这里
+    var fileString = evt.target.result; // 读取文件内容
+    console.log(fileString);
+  };
+  File.upload(formData.get("file"))
 }
 </script>
 
 <template>
-  <div class="w-full flex h-full bg-white select-none items-center justify-center border-8 rounded-r-xl rounded-l-xl shadow-lg">
+  <div
+    class="w-full flex h-full bg-white select-none items-center justify-center border-8 rounded-r-xl rounded-l-xl shadow-lg"
+  >
     <div class="flex flex-col">
       <div class="text-center font-medium my-2">请选择文件上传</div>
       <div>
         <input
           type="file"
-          id = "file"
-          name = "file"
+          id="file"
+          name="file"
           class="block w-full text-sm text-slate-500 file:mr-8 file:py-2 file:px-10 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100 hover:cursor-pointer"
         />
       </div>
