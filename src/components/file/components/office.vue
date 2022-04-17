73d85055839e6b592e3f859e855ee4bfa411b489
Liu Ruohui <1829504353@qq.com>
2022-04-17 20:50:32 +0800

    测试

--- src/components/file/components/office.vue
+++ src/components/file/components/office.vue
@@ -4,6 +4,6 @@
 
 <template>
   <div class="w-full m-8 border-2 border-red-100">
-    <iframe src="" frameborder="0" class="w-full h-full"></iframe>
+    <iframe src="http://api.jinzh.me/upload/1510426919512838144-readme.txt" frameborder="0" class="w-full h-full select-none"></iframe>
   </div>
 </template>
