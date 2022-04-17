8bc4fd1f71eb014dc82316149886d949ec018b18
Liu Ruohui <1829504353@qq.com>
2022-04-17 15:20:47 +0800

    文件微调

--- src/composables/onlogined.js
+++ src/composables/onlogined.js
@@ -1,9 +1,10 @@
 // 声明周期，在登录完成后执行
 
-import { Note, Infos } from "./api";
+import { Note, Infos, File } from "./api";
 
 function onlogined() {
-	Note.getIndex();
-	Infos.getInfo();
+    Note.getIndex();
+    Infos.getInfo();
+    File.getIndex();
 }
 export default onlogined;
