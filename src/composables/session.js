d033a992a892998c47c79a00ccb6a82cdd2609ef
yingyi <jinzhongjia@qq.com>
2022-04-19 15:33:30 +0800

    sessionid重新封装，tool中引入localstroage管理

--- src/composables/session.js
+++ src/composables/session.js
@@ -1,22 +1,26 @@
 /*
 创建session.js 存在一个对象两个方法分别为读取和修改 外部不暴露session 通过对象的方法访问 暴露对象在外
  */
+import { Storage } from "./tool";
 
-// 创建session，通过localstroage获取
-var sessionId = localStorage.getItem("Session-Id");
+//localStorage
+const ls = new Storage();
 
+//有效时间为： 1.5小时
+const expired = 1.5 * 60 * 60 * 1000;
+
+//对外暴露的session对象
 var session = {};
 
 //方法设置sessionId
-session.setSessionId = function (tmp) {
+session.setSessionId = function (value) {
 	//设置sessionId
-	sessionId = tmp;
-	localStorage.setItem("Session-Id", tmp);
+	ls.set("Session-Id", value, expired);
 };
 
 // 方法获取sessionId
 session.getSessionId = function () {
-	return sessionId;
+	return ls.get("Session-Id");
 };
 
 export { session };
