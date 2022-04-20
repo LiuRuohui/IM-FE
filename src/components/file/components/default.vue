6c2f0529e09f21900e11c3a5079cb942901e8a96
Liu Ruohui <1829504353@qq.com>
2022-04-20 22:02:31 +0800

    删除无用代码

--- src/components/file/components/default.vue
+++ src/components/file/components/default.vue
@@ -1,14 +1,11 @@
 <script setup>
 import { filePreview } from "../../../composables/data/file";
-function download() {
-  console.log("Downloading");
-}
 </script>
 
 <template>
   <div class="w-full justify-center flex">
     <a :href="filePreview.Url">
-      <button @click="download" class="btn top-1/2 relative">点击下载</button>
+      <button class="btn top-1/2 relative">点击下载</button>
     </a>
   </div>
 </template>
