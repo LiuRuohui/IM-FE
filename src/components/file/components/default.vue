269055897237d0c50e68eeff64528158b7a2e59f
Liu Ruohui <1829504353@qq.com>
2022-04-26 00:24:58 +0800

    文件下载小更改

--- src/components/file/components/default.vue
+++ src/components/file/components/default.vue
@@ -1,11 +1,26 @@
 <script setup>
 import { filePreview } from "../../../composables/data/file";
+function upload() {
+  let link = document.createElement("a");
+  //设置下载的文件名
+  link.download = filePreview.Name;
+  link.style.display = "none";
+  //设置下载路径
+  link.href = filePreview.Url;
+  console.log(link.download);
+  console.log(link.href);
+  //触发点击
+  document.body.appendChild(link);
+  link.click();
+  //移除节点
+  document.body.removeChild(link);
+}
 </script>
 
 <template>
   <div class="w-full justify-center flex">
-    <a :href="filePreview.Url">
-      <button class="btn top-1/2 relative">点击下载</button>
-    </a>
+    <div>
+      <button class="btn top-1/2 relative" @click="upload">点击下载</button>
+    </div>
   </div>
 </template>
