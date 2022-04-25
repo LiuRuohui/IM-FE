23c747d9b832220a110a359b97b720192e7811e5
Liu Ruohui <1829504353@qq.com>
2022-04-25 19:24:42 +0800

    初始化message和apply

--- src/composables/websocket/message.js
+++ src/composables/websocket/message.js
@@ -1,1 +1,35 @@
 //存储发送的消息
+import http from "../http"
+
+const singleChat = {
+    data: [],
+    getFriends,
+    getOneFriend,
+}
+
+//获取好友列表
+function getFriends() {
+
+}
+
+//获取单个好友信息
+function getOneFriend() {
+
+}
+
+
+const groupChat = {
+        data: [],
+        getAllGroups,
+        createGroup
+    }
+    //获取群组列表
+function getAllGroups() {
+
+}
+//创建群聊
+function createGroup() {
+
+}
+
+export { singleChat, groupChat }
