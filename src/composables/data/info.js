fec58b34112beca6d2d3de92ded3004323b7b67d
yingyi <jinzhongjia@qq.com>
2022-04-27 17:17:30 +0800

    同步

--- src/composables/data/info.js
+++ src/composables/data/info.js
@@ -7,71 +7,74 @@ import Global from "../global";
 
 // 原始的info
 const info = reactive({
-  data: {},
-  getInfo,
-  global,
-  getUser,
+	data: {},
+	getInfo,
+	global,
+	getUser,
 });
 
 //拷贝
 const Info = reactive({
-  data: {},
-  getInfos,
+	data: {},
+	getInfos,
 });
 
 function getInfo() {
-  http.get("/user/info", {}).then(
-    (data) => {
-      info.data = data;
-      console.log("获取信息成功了", data);
-    },
-    (error) => {
-      console.log("获取信息失败了", error);
-    }
-  );
+	http.get("/user/info", {}).then(
+		(data) => {
+			info.data = data;
+			console.log("获取信息成功了", data);
+		},
+		(error) => {
+			console.log("获取信息失败了", error);
+		}
+	);
 }
 
 function global() {
-  http.get("/user/info", {}).then(
-    (data) => {
-      info.data = data;
-      console.log("获取信息成功了", data);
-      Global();
-    },
-    (error) => {
-      console.log("获取信息失败了", error);
-    }
-  );
+	http.get("/user/info", {}).then(
+		(data) => {
+			info.data = data;
+			console.log("获取信息成功了", data);
+			Global();
+		},
+		(error) => {
+			console.log("获取信息失败了", error);
+		}
+	);
 }
 
 function getInfos() {
-  http.get("/user/info", {}).then(
-    (data) => {
-      Info.data = data;
-      router.push({
-        name: "setting",
-      });
-    },
-    (error) => {
-      console.log("获取信息失败了", error);
-      // 出错时直接跳转会首页
-      // router.push({
-      //     name: "login",
-      // })
-    }
-  );
+	http.get("/user/info", {}).then(
+		(data) => {
+			Info.data = data;
+			router.push({
+				name: "setting",
+			});
+		},
+		(error) => {
+			console.log("获取信息失败了", error);
+			// 出错时直接跳转会首页
+			// router.push({
+			//     name: "login",
+			// })
+		}
+	);
 }
 
 //获取其他用户的信息
-function getUser(userId) {
-  http.post("/user/getData", { userId: userId }).then(
-    (data) => {
-      console.log("获取其他用户成功", data);
-    },
-    (error) => {
-      console.log("获取其他用户失败", error);
-    }
-  );
+async function getUser(userId) {
+	let tmp;
+	await http.post("/user/getData", { userId: userId }).then(
+		(data) => {
+			console.log("获取其他用户成功", data);
+			tmp = data;
+		},
+		(error) => {
+			console.log("获取其他用户失败", error);
+		}
+	);
+	return tmp;
 }
 
 export { info, Info };
