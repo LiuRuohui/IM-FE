c5fba535186c7525eaeaf78c0951fcc9a16cf5e1
yingyi <jinzhongjia@qq.com>
2022-04-30 10:59:08 +0800

    bug修复，protobuf打包问题

--- src/composables/websocket/msg.js
+++ src/composables/websocket/msg.js
@@ -1,89 +1,88 @@
 /*eslint-disable block-scoped-var, id-length, no-control-regex, no-magic-numbers, no-prototype-builtins, no-redeclare, no-shadow, no-var, sort-vars*/
-import * as $protobuf from "protobufjs/light";
+"use strict";
 
-const $root = (
-  $protobuf.roots["default"] ||
-  ($protobuf.roots["default"] = new $protobuf.Root())
-).addJSON({
-  proto: {
-    options: {
-      go_package: "../",
-    },
-    nested: {
-      Body: {
-        fields: {
-          type: {
-            type: "int32",
-            id: 1,
-          },
-          msg: {
-            type: "Msg",
-            id: 2,
-          },
-          apply: {
-            type: "Apply",
-            id: 3,
-          },
-        },
-      },
-      Msg: {
-        fields: {
-          end: {
-            type: "string",
-            id: 1,
-          },
-          groupId: {
-            type: "string",
-            id: 2,
-          },
-          extra: {
-            type: "string",
-            id: 3,
-          },
-          type: {
-            type: "int32",
-            id: 4,
-          },
-          msg: {
-            type: "string",
-            id: 5,
-          },
-          time: {
-            type: "int64",
-            id: 6,
-          },
-        },
-      },
-      Apply: {
-        fields: {
-          end: {
-            type: "string",
-            id: 1,
-          },
-          time: {
-            type: "int64",
-            id: 2,
-          },
-          type: {
-            type: "int32",
-            id: 3,
-          },
-          deal: {
-            type: "string",
-            id: 4,
-          },
-          groupId: {
-            type: "string",
-            id: 5,
-          },
-          reason: {
-            type: "string",
-            id: 6,
-          },
-        },
-      },
-    },
-  },
+import { roots, Root } from "protobufjs/dist/light/protobuf.min";
+
+var $root = (roots["default"] || (roots["default"] = new Root())).addJSON({
+	proto: {
+		options: {
+			go_package: "../",
+		},
+		nested: {
+			Body: {
+				fields: {
+					type: {
+						type: "int32",
+						id: 1,
+					},
+					msg: {
+						type: "Msg",
+						id: 2,
+					},
+					apply: {
+						type: "Apply",
+						id: 3,
+					},
+				},
+			},
+			Msg: {
+				fields: {
+					end: {
+						type: "string",
+						id: 1,
+					},
+					groupId: {
+						type: "string",
+						id: 2,
+					},
+					extra: {
+						type: "string",
+						id: 3,
+					},
+					type: {
+						type: "int32",
+						id: 4,
+					},
+					msg: {
+						type: "string",
+						id: 5,
+					},
+					time: {
+						type: "int64",
+						id: 6,
+					},
+				},
+			},
+			Apply: {
+				fields: {
+					end: {
+						type: "string",
+						id: 1,
+					},
+					time: {
+						type: "int64",
+						id: 2,
+					},
+					type: {
+						type: "int32",
+						id: 3,
+					},
+					deal: {
+						type: "string",
+						id: 4,
+					},
+					groupId: {
+						type: "string",
+						id: 5,
+					},
+					reason: {
+						type: "string",
+						id: 6,
+					},
+				},
+			},
+		},
+	},
 });
 
-export { $root as default };
+export default $root;
